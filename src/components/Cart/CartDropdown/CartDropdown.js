4c4866a82da74867f919b73dea30bbc3b37133e4
kj-ninja <jakubiak.krzy@gmail.com>
2020-08-04 10:15:57 +0200

    added reselect lib to app and updated cart items with new selectors

--- src/components/Cart/CartDropdown/CartDropdown.js
+++ src/components/Cart/CartDropdown/CartDropdown.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {selectCartItems} from "../../../store/selectors/cart";
 import './CartDropdown.scss';
 import CustomButton from "../../../UI/CustomButton/CustomButton";
 import CartItem from "../CartItem/CartItem";
@@ -17,8 +18,8 @@ const CartDropdown = ({cartItems}) => {
     );
 };
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/Cart/CartIcon/CartIcon.js
+++ src/components/Cart/CartIcon/CartIcon.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {selectCartItemsCount} from '../../../store/selectors/cart';
 import {toggleCartHidden} from "../../../store/actions/cart";
 import './CartIcon.scss';
 import {ReactComponent as ShoppingIcon} from '../../../assets/shopping-bag.svg'
 
-const CartIcon = ({toggleCartHidden}) => {
+const CartIcon = ({toggleCartHidden, itemCount}) => {
     return (
         <div className="cart-icon" onClick={toggleCartHidden}>
             <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
+            <span className="item-count">{itemCount}</span>
         </div>
     );
 };
 
-export default connect(null, {toggleCartHidden})(CartIcon);
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+})
+
+export default connect(mapStateToProps, {toggleCartHidden})(CartIcon);
